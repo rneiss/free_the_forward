2d68ebd6116adf62057f976ff414852ae307666d
Russel Neiss <rneiss@users.noreply.github.com>
2020-10-01 19:21:04 -0500

    I am a horribly petty person...

--- free_the_forward.js
+++ free_the_forward.js
@@ -1,47 +1,57 @@
 chrome.webRequest.onBeforeRequest.addListener(
-  function (details) {
-    var url_parts = details.url.split('?');
-    if (url_parts[1]) {
-      return {
-        redirectUrl: url_parts[0]
-      }
-    }
-  },
-  {
-    urls: [
-      'http://*.forward.com/*',
-      'https://*.forward.com/*',
-    ]
-  },
-  [
-    'blocking',
-  ]
-);
-
+        function(details) {
+          console.log("free the forward blocking  " + details.url);
+          return {cancel: true};
+        },
+        {urls: [
+          "*://*.lightboxcdn.com/*",
+          ] },
+        ["blocking"]);
 
 chrome.webRequest.onBeforeSendHeaders.addListener(
-  function (details) {
-    for (var i = 0; i < details.requestHeaders.length; ++i) {
-      if (details.requestHeaders[i].name === 'User-Agent') {
-        details.requestHeaders[i].value = 'Googlebot/2.1 (+http://www.googlebot.com/bot.html)';
-        break;
-      }
-    }
+        function(details) {
+          for (var i = 0; i < details.requestHeaders.length; ++i) {
+            if (details.requestHeaders[i].name === 'User-Agent') {
+              details.requestHeaders[i].value = "Googlebot/2.1 (+http://www.googlebot.com/bot.html)";
+              break;
+            }
+          }
+          return {requestHeaders: details.requestHeaders};
+        },
+        {urls: [
+          "*://forward.com/*",
+        ]},
+        ["blocking", "requestHeaders"]);
 
-    return {
-      requestHeaders: details.requestHeaders
-    };
-  },
-  {
-    urls: [
-      'http://*.forward.com/*',
-      'https://*.forward.com/*',
-    ]
-  },
-  [
-    'blocking',
-    'requestHeaders'
-  ]
-);
+chrome.webRequest.onBeforeSendHeaders.addListener(
+        function(details) {
+          for (var i = 0; i < details.requestHeaders.length; ++i) {
+            if (details.requestHeaders[i].name === 'Referer') {
+              details.requestHeaders.splice(i, 1);
+              break;
+            }
+          }
+          return {requestHeaders: details.requestHeaders};
+        },
+        {urls: [
+          "*://forward.com/*",
+        ] },
+        ["blocking", "requestHeaders"]);
 
+chrome.webRequest.onBeforeSendHeaders.addListener(
+        function(details) {
+          var j = 0;
+          for (var i = 0; i < details.requestHeaders.length; ++i) {
+            if (details.requestHeaders[i].name === 'Cookie') {
+              details.requestHeaders.splice(i, 1);
+              j = j+1;
+            }
+            if( j > 0 ) { console.log("Cookies : " + j); }
+          }
+          return {requestHeaders: details.requestHeaders};
+        },
+        {urls: [
+          "*://forward.com/*",
+        ] },
+        ["blocking", "requestHeaders"]);
 
